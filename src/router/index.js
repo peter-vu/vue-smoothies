e5df6755cf350d0552fbb8be320857a33422bb87
Peter Vu <vutuananh.vn.sg@gmail.com>
2020-12-04 23:21:57 +0800

    Commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddSmoothie from '@/components/AddSmoothie'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'Index',
-        component: Index
-    }]
+            path: '/',
+            name: 'Index',
+            component: Index
+        },
+        {
+            path: '/AddSmoothie',
+            name: 'AddSmoothie',
+            component: AddSmoothie
+        }
+
+    ]
 })
