b14cf81d4894da398b41d1c35c1a70a2f3109104
Peter Vu <vutuananh.vn.sg@gmail.com>
2020-12-06 22:00:01 +0800

    Update smoothie

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,9 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
-        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)"
-          >delete</i
-        >
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -12,6 +10,11 @@
           </li>
         </ul>
       </div>
+      <span class="btn-floating btn-large halfway-fab pink">
+        <router-link :to="{name:'EditSmoothie', params:{smoothie_slug:smoothie.slug}}">
+          <i class="material-icons edit">edit</i>
+        </router-link>
+      </span>
     </div>
   </div>
 </template>
